7a64e77242d76b2eb1425f945402430a875fa5e6
Jose Rene Familia <familiajoserene@gmail.com>
2023-10-01 12:05:26 -0400

    Modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import logo from './../assets/Images/logodisney.png'
+import logo from './../assets/images/logo.png'
 import { HiHome,
     HiMagnifyingGlass,
     HiStar,
